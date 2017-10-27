ef1674356c9262e24fec53c2ad70b490b48b7f50
Dmitriy Sadkovoy <d.sadkovoy@smartweb.com.ua>
2017-10-27 18:37:46 +0300

    Removed comments from seeds and migrations

--- src/seeders/20171027114916-example-flags.js
+++ src/seeders/20171027114916-example-flags.js
@@ -1,15 +1,5 @@
 module.exports = {
     up: (queryInterface, Sequelize) => {
-        /*
-          Add altering commands here.
-          Return a promise to correctly handle asynchronicity.
-
-          Example:
-          return queryInterface.bulkInsert('Person', [{
-            name: 'John Doe',
-            isBetaMember: false
-          }], {});
-        */
         return queryInterface.bulkInsert('feature_flags', [{
             name: 'MY_NEW_COOL_FEATURE',
             enabled: false,
@@ -20,13 +10,6 @@ module.exports = {
     },
 
     down: (queryInterface, Sequelize) => {
-        /*
-          Add reverting commands here.
-          Return a promise to correctly handle asynchronicity.
-
-          Example:
-          return queryInterface.bulkDelete('Person', null, {});
-        */
         return queryInterface.bulkDelete('feature_flags', null, {});
     }
 };
